5681da2abb5985fe2745d88b0e45e7a78b2ed358
yuukisakai54 <yuukisakai54@gmail.com>
2024-11-13 21:46:47 +0900

    Add program to reverse string using command line arguments

---
+++ MyProject/src/mypackage/index.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class index {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String str = args[0]; // コマンドライン引数として受け取った文字列
+            String reversedStr = reverseString(str);
+
+            System.out.println("元の文字列: " + str);
+            System.out.println("逆順の文字列: " + reversedStr);
+        } else {
+            System.out.println("文字列を引数として渡してください。");
+        }
+    }
+
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+}
